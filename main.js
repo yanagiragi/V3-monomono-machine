8d30ca019e1c5228f65b21e9001559d4d8c56f4a
yanagiragi <qaz90473@gmail.com>
2017-02-18 17:33:37 +0800

    Init

---
+++ main.js
@@ -0,0 +1,48 @@
+var request = require('request')
+var cookie = require('cookie')
+var decode = require('urlencode')
+
+var base = 1482067126359
+var str = 'http://www.spike-chunsoft.co.jp/monomono-machine/gacha?u='
+
+next()
+
+function next(){
+	
+	request('http://www.spike-chunsoft.co.jp/monomono-machine/todaymedal?u=1482150015368', (err, res, body) => {
+		
+		var jar = cookie.serialize(res.headers['set-cookie']);
+		var setcookie = cookie.serialize(res.headers['set-cookie'])
+		setcookie = setcookie.substr(setcookie.indexOf('=') + 1)
+		setcookie = setcookie.substr(0, setcookie.indexOf(';'))
+		var datas = {
+			url : str + base.toString(),
+			headers : {
+				//'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.3',
+				//'content-type' : 'application/x-www-form-urlencoded',
+				'Cookie' : 'sound_muted=%7B%22_v%22%3Afalse%7D; nlbi_346135=bbqMdD9BQi3H1mOPNPXIJAAAAADuGshpKojiQsiZHRblUzZg; incap_ses_573_346135=CsJPWASGNGCzmLAAfrTzB1fLV1gAAAAA3V8JVluO5Bdb4LA15OXYRA==; incap_ses_571_346135=Dg8mfmuNEF/dFebsgpnsBxLMV1gAAAAArC0zN8f76GOkTs27N8Btxw==; AWSELB=6D3B5F6F021FBAC18FD480463C44188E9DF91D875BC9885BE24C2144BCDC4373595AD48091A215AA71A8B1033F41EB2C570BC6229EAC38DB32F083AD64BE1946058303DE2E; visid_incap_346135=en/SzxLQQh+wEc1A74XP2FfLV1gAAAAAQUIPAAAAAACob56qXcpvSc2TSLMdPPhx; incap_ses_576_346135=gNgxbfK2yQDJ1T33+Vz+B3XNV1gAAAAAsHUFDgpjwnaPJY9uMim1Kg==; _gat=1; drv3_userid='
+				+ setcookie + '; _ga=GA1.3.1800313649.1482149239'
+			}
+		}
+		//console.log(base.toString())
+		//base --
+		request(datas, (err, res, body) => {			
+			var str = body.toString()
+			str = str.substr(str.indexOf('</div>') + 6)
+			var data = JSON.parse(str)
+			console.log(data.data.image);
+			if(data.data.image.indexOf('thumb_sns_18') != -1){
+				console.log('Found!')
+				console.log(datas);
+				process.exit()
+			}
+			else{
+				next()
+			}
+		})
+	})
+	
+	//break
+
+	//if(base == 1482067126359 - 60) break;
+}
